b7ed37663c86a5807bc0e25864e801b9e5d0531d
MagedSaeed <msmj2015@gmail.com>
2020-07-04 13:03:57 +0300

    adding subwords tokenizer. Adding points for discussion

--- tokenizers.py
+++ tokenizers.py
@@ -12,14 +12,23 @@ import sentencepiece as spm
 from collections import defaultdict, Counter
 from farasa.segmenter import FarasaSegmenter
 
+
 class BaseTokenizer:
     """
     Base Tokenizer that implements the basic functionalities of a tokenizer
     """
-    def __init__(self,  unknown_token = "<UNK>", padding_token = "<PAD>",
-                        segment = False, max_tokens = 10000,
-                        segm_token = '+', split = True, 
-                        clean = False, normalize = False):
+
+    def __init__(
+        self,
+        unknown_token="<UNK>",
+        padding_token="<PAD>",
+        segment=False,
+        max_tokens=10000,
+        segm_token="+",
+        split=True,
+        clean=False,
+        normalize=False,
+    ):
         """Constructor
 
         Args:
@@ -40,17 +49,18 @@ class BaseTokenizer:
         self.clean = clean
         self.normalize = normalize
         self.split = split
-        self.norm_dict = pickle.load(open("normalization_dictionary.pl", 'rb'))
-        
+        self.norm_dict = pickle.load(open("normalization_dictionary.pl", "rb"))
+
         if self.segment:
-            print('Initializing Farasa')
+            print("Initializing Farasa")
             # suppress farasa stdout
+            # WARNING: this is LINUX ONLY command!
             old_stdout = sys.stdout
             sys.stdout = open(os.devnull, "w")
-            self.segmenter = FarasaSegmenter(interactive = True)
+            self.segmenter = FarasaSegmenter(interactive=True)
             # resume farasa stdout
             sys.stdout = old_stdout
-            
+
     def process_data(self, file_path):
         """ 
         Read, segment, clean, normalize and split
@@ -59,15 +69,15 @@ class BaseTokenizer:
             file_path (str): the directory of the data to read
         
         """
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             print("Reading the data ...")
             self.corpus = f.read()
-        
+
         if self.segment:
             print("Segmenting the data ...")
             self.corpus = self.segmenter.segment(self.corpus)
             self.corpus = re.sub(r"[+]", self.segm_token, self.corpus)
-        
+
         if self.clean:
             print("Cleaning the data ...")
             self.corpus = clean_data(self.corpus)
@@ -78,11 +88,11 @@ class BaseTokenizer:
 
         if self.split:
             print("Splitting the data ...")
-            Path("data/raw").mkdir(parents = True, exist_ok = True)
+            Path("data/raw").mkdir(parents=True, exist_ok=True)
             self.train_text, self.valid_text, self.test_text = self._split_corpus()
             self._write_data("data/raw/train.txt", self.train_text)
             self._write_data("data/raw/valid.txt", self.valid_text)
-            self._write_data("data/raw/test.txt",  self.test_text)
+            self._write_data("data/raw/test.txt", self.test_text)
             del self.train_text, self.valid_text, self.test_text
             del self.corpus
 
@@ -97,23 +107,23 @@ class BaseTokenizer:
             Dict: frequency based dictionary   
         """
         encoding = "utf8"
-        with open(file_path, 'r', encoding=encoding, errors='ignore') as f:
+        with open(file_path, "r", encoding=encoding, errors="ignore") as f:
             with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as m:
                 m.read(0)
-                i = 0 
+                i = 0
                 size_to_read = int(1e9)
                 freq = Counter([])
-                pbar = tqdm(total=int(m.size()/size_to_read))
+                pbar = tqdm(total=int(m.size() / size_to_read))
                 while i < m.size():
                     cur_txt = ""
                     data = m.read(size_to_read)
-                    i+= size_to_read
+                    i += size_to_read
                     try:
-                        cur_txt= data.decode(encoding)
+                        cur_txt = data.decode(encoding)
                     except:
-                        cur_txt= (data + m.read(1)).decode(encoding) 
-                        i +=1 
-                    freq.update(cur_txt.split(' '))
+                        cur_txt = (data + m.read(1)).decode(encoding)
+                        i += 1
+                    freq.update(cur_txt.split(" "))
                     pbar.update(1)
         return freq
 
@@ -125,6 +135,7 @@ class BaseTokenizer:
             file_path (str): the directory of the data to read
         
         """
+        # TOCHECK: I think this code will break if the path does not exist.
         open(path, "w").write(data)
 
     def _split_corpus(self):
@@ -134,12 +145,18 @@ class BaseTokenizer:
         Returns:
             Tuple: train, valid, test
         """
-        split_length = int(len(self.corpus) * .8)
-        trainval_text, test_text = self.corpus[:split_length], self.corpus[split_length:]
-        split_length = int(len(trainval_text) * .8)
-        train_text, val_text = trainval_text[:split_length], trainval_text[split_length:]
+        split_length = int(len(self.corpus) * 0.8)
+        trainval_text, test_text = (
+            self.corpus[:split_length],
+            self.corpus[split_length:],
+        )
+        split_length = int(len(trainval_text) * 0.8)
+        train_text, val_text = (
+            trainval_text[:split_length],
+            trainval_text[split_length:],
+        )
         return train_text, val_text, test_text
-    
+
     def _get_tokens_frequency(self, file_path):
         """
         Get tokens frequency using a dictionary
@@ -149,13 +166,13 @@ class BaseTokenizer:
         Returns:
             dict : dict containing frequency
         """
-        text = open(file_path, 'r').read()
+        text = open(file_path, "r").read()
         tokens_frequency = defaultdict(int)
         for word in text.split(" "):
-            tokens_frequency[word]+=1
+            tokens_frequency[word] += 1
         return dict(tokens_frequency)
-    
-    def _split_word(self, word, number_of_subwords): 
+
+    def _split_word(self, word, number_of_subwords):
         """Split a word into a specific number of sub-words
 
         Args:
@@ -165,29 +182,33 @@ class BaseTokenizer:
         Returns:
             list: list of subwords 
         """
-        assert number_of_subwords>1
+        assert number_of_subwords > 1
 
-        # groups_of_subwords = [] 
-        def _split(_word, _number_of_subwords): 
-            groups = [] 
-            if _number_of_subwords==1 or len(_word) == 1: 
-                groups.append(["##"+_word]) 
-            else: 
-                for i in range(1, len(_word), 1): 
-                    groups.extend((_word[:i], *group) for group in _split(_word[i:],_number_of_subwords-1) if len(group)==_number_of_subwords-1) 
+        # groups_of_subwords = []
+        def _split(_word, _number_of_subwords):
+            groups = []
+            if _number_of_subwords == 1 or len(_word) == 1:
+                groups.append(["##" + _word])
+            else:
+                for i in range(1, len(_word), 1):
+                    groups.extend(
+                        (_word[:i], *group)
+                        for group in _split(_word[i:], _number_of_subwords - 1)
+                        if len(group) == _number_of_subwords - 1
+                    )
                 # groups_of_subwords = groups
-            return groups 
-         
-        groups_of_subwords = _split(word,number_of_subwords)
-        
+            return groups
+
+        groups_of_subwords = _split(word, number_of_subwords)
+
         return groups_of_subwords
-    
+
     def encode(self, text):
         """
         Convert text to ids 
         """
         return NotImplementedError
-    
+
     def decode(self, encoded):
         """
         Convert ids to string
@@ -209,17 +230,23 @@ class BaseTokenizer:
     def encode_and_save(self):
         """Encode all the files then save as numpy
         """
-        Path("data/encoded").mkdir(parents = True, exist_ok = True)
-        for file_path in os.listdir('data/raw/'):
-            ids = self.encode(open(f"data/raw/{file_path}", 'r').read())
-            np.save(f'data/encoded/{file_path[:-4]}.npy', ids)
+        Path("data/encoded").mkdir(parents=True, exist_ok=True)
+        for file_path in os.listdir("data/raw/"):
+            ids = self.encode(open(f"data/raw/{file_path}", "r").read())
+            np.save(f"data/encoded/{file_path[:-4]}.npy", ids)
 
+
+# I am not sure we should call this FREQUENCY as we
+# are not using frequencies here. Its name is misleading.
+# The auto tokenizer should be named a frequency tokenizer because
+# it uses the frequency table. What do you think?
 class FrequencyTokenizer(BaseTokenizer):
     """ Frequency based tokenization 
     """
-    tokens_frequency = None 
 
-    def train(self, large_file = False):
+    tokens_frequency = None
+
+    def train(self, large_file=False):
         """Train data using tokens' frequency
 
         Args:
@@ -227,25 +254,29 @@ class FrequencyTokenizer(BaseTokenizer):
         """
         if large_file:
             sorted_tokens_frequency = {
-                    k:v for k,v in sorted(
-                            self._get_tokens_frequency_quickly('data/raw/train.txt').items(),
-                            key=lambda x: x[1],
-                            reverse=True
-                            )
-                        }
+                k: v
+                for k, v in sorted(
+                    self._get_tokens_frequency_quickly("data/raw/train.txt").items(),
+                    key=lambda x: x[1],
+                    reverse=True,
+                )
+            }
         else:
             sorted_tokens_frequency = {
-                        k:v for k,v in sorted(
-                                self._get_tokens_frequency('data/raw/train.txt').items(),
-                                key=lambda x: x[1],
-                                reverse=True
-                                )
-                            }
+                k: v
+                for k, v in sorted(
+                    self._get_tokens_frequency("data/raw/train.txt").items(),
+                    key=lambda x: x[1],
+                    reverse=True,
+                )
+            }
 
         limited_tokens_frequency = dict()
         limited_tokens_frequency[self.unknown_token] = -1
         limited_tokens_frequency[self.padding_token] = -1
-        limited_tokens_frequency.update({k:v for k,v in list(sorted_tokens_frequency.items())[:self.max_tokens]})
+        limited_tokens_frequency.update(
+            {k: v for k, v in list(sorted_tokens_frequency.items())[: self.max_tokens]}
+        )
         self.tokens_frequency = limited_tokens_frequency
 
     def load_model(self, file_path):
@@ -254,8 +285,8 @@ class FrequencyTokenizer(BaseTokenizer):
         Args:
             file_path (str): file path of the dictionary
         """
-        print('Loading as pickle file ...')
-        self.tokens_frequency = pickle.load(open(file_path, 'rb'))
+        print("Loading as pickle file ...")
+        self.tokens_frequency = pickle.load(open(file_path, "rb"))
 
     def save_model(self, file_path):
         """Save a model as a freqency dictionary
@@ -264,8 +295,8 @@ class FrequencyTokenizer(BaseTokenizer):
             file_path (str): file path to save the model
         """
         assert self.tokens_frequency
-        with open(f'{file_path}', 'wb') as pickle_file:
-            print('Saving as pickle file ...')
+        with open(f"{file_path}", "wb") as pickle_file:
+            print("Saving as pickle file ...")
             pickle.dump(self.tokens_frequency, pickle_file)
 
     def tokenize(self, text):
@@ -281,11 +312,12 @@ class FrequencyTokenizer(BaseTokenizer):
         output_tokens = []
         for word in text.split():
             if word in self.tokens_frequency.keys():
-                output_tokens.append(word) 
+                output_tokens.append(word)
             else:
                 output_tokens.append(self.unknown_token)
         return output_tokens
-    
+
+    # Why do we have two versions of this method?
     def _tokens_list(self):
         """ Get tokens list
 
@@ -313,8 +345,8 @@ class FrequencyTokenizer(BaseTokenizer):
         """
         decoded = [self.tokens_list()[id] for id in encoded]
         return decoded
-    
-    def encode(self,text):
+
+    def encode(self, text):
         """ Convert string to a list of ids
 
         Args:
@@ -336,26 +368,32 @@ class FrequencyTokenizer(BaseTokenizer):
         Returns:
             str: detokenized string
         """
-        detokenized = ''.join(tokens).replace('','')
+        detokenized = "".join(tokens).replace("", "")
         return detokenized
+
 
 class SentencePieceTokenizer(BaseTokenizer):
     """ Sentencepiece based tokenization. 
     """
-    def train(self, model_type= "bpe"):
+
+    def train(self, model_type="bpe"):
         """ Train using sentence piece
 
         Args:
             model_type (str, optional): train using sp. Defaults to "bpe".
         """
         self.model = io.BytesIO()
-        spm.SentencePieceTrainer.train(input= 'data/raw/train.txt', 
-                                        model_writer=self.model, vocab_size=self.max_tokens, 
-                                        model_type = model_type, character_coverage=1.0, 
-                                        normalization_rule_name='identity')
+        spm.SentencePieceTrainer.train(
+            input="data/raw/train.txt",
+            model_writer=self.model,
+            vocab_size=self.max_tokens,
+            model_type=model_type,
+            character_coverage=1.0,
+            normalization_rule_name="identity",
+        )
         self.save_model("m.model")
-        self.sp = spm.SentencePieceProcessor(model_file = 'm.model')
-    
+        self.sp = spm.SentencePieceProcessor(model_file="m.model")
+
     def tokenize(self, text):
         """Tokenize using the frequency dictionary 
 
@@ -365,7 +403,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         Returns:
             list: generated tokens
         """
-        return self.sp.encode(text, out_type = str)
+        return self.sp.encode(text, out_type=str)
 
     def load_model(self, file_path):
         """Load a saved sp model
@@ -382,7 +420,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         Args:
             file_path (str): file path to save the model
         """
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             f.write(self.model.getvalue())
 
     def encode(self, text):
@@ -394,7 +432,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         Returns:
             list: list of ids
         """
-        return self.sp.encode(text, out_type = int)
+        return self.sp.encode(text, out_type=int)
 
     def decode(self, encoded):
         """ Decode ids
@@ -416,22 +454,25 @@ class SentencePieceTokenizer(BaseTokenizer):
         Returns:
             str: detokenized string
         """
-        return ''.join(tokens).replace('▁', ' ')
+        return "".join(tokens).replace("▁", " ")
+
 
 class AutoTokenizer(BaseTokenizer):
     """ Auto tokenization using a saved dictionary 
     """
-    def __init__(self, vocab = 'vocab.pl'):
+
+    def __init__(self, vocab="vocab.pl"):
         """Tokenize and segment without training
 
         Args:
             vocab (str, optional): pickled vocabulary for tokenization. Defaults to 'vocab.pl'.
         """
         print("loading default vocab ...")
-        self.vocab = pickle.load(open(vocab, 'rb'))
+        # CHECKIT: maybe we need to allow the user to put another path?
+        # Otherwise, we should not put it in the constructor
+        self.vocab = pickle.load(open(vocab, "rb"))
         super().__init__(self)
 
- 
     def tokenize(self, text):
         """Tokenize using the frequency dictionary 
 
@@ -448,20 +489,23 @@ class AutoTokenizer(BaseTokenizer):
             if word in self.vocab.keys():
                 output_tokens.append(word)
             else:
-                for i in range(2,len(word)+1,1):
-                    groups_of_valid_subwords = self._split_word(word,i)
+                for i in range(2, len(word) + 1, 1):
+                    groups_of_valid_subwords = self._split_word(word, i)
                     if groups_of_valid_subwords:
                         break
-                if len(groups_of_valid_subwords)==0:
+                if len(groups_of_valid_subwords) == 0:
                     output_tokens.append(self.unknown_token)
                 else:
-                    sorted_groups_of_valid_subwords = sorted(groups_of_valid_subwords, key=lambda group: sum(self.vocab[subword] for subword in group))
-                    
+                    sorted_groups_of_valid_subwords = sorted(
+                        groups_of_valid_subwords,
+                        key=lambda group: sum(self.vocab[subword] for subword in group),
+                    )
+
                     tokens = sorted_groups_of_valid_subwords[-1]
                     for token in tokens:
                         output_tokens.append(str(token))
         return output_tokens
-    
+
     def _tokens_list(self):
         """ Get tokens list
 
@@ -469,7 +513,7 @@ class AutoTokenizer(BaseTokenizer):
             list: list of tokens.
         """
         return list(self.vocab.keys())
-    
+
     def decode(self, encoded):
         """ Decode ids
 
@@ -481,8 +525,8 @@ class AutoTokenizer(BaseTokenizer):
         """
         decoded = [self._tokens_list()[id] for id in encoded]
         return decoded
-    
-    def encode(self,text):
+
+    def encode(self, text):
         """ Convert string to a list of ids
 
         Args:
@@ -491,10 +535,11 @@ class AutoTokenizer(BaseTokenizer):
         Returns:
             list: list of ids
         """
+        # TOCKECK: Why not to put this in the base tokenizer as a default behaviour?
         tokens = self.tokenize(text)
         encoded = [self._tokens_list().index(token) for token in tokens]
         return encoded
-    
+
     def detokenize(self, tokens):
         """ Convert tokens to a string
 
@@ -504,5 +549,61 @@ class AutoTokenizer(BaseTokenizer):
         Returns:
             str: detokenized string
         """
-        detokenized = ''.join(tokens).replace('##','')
+        detokenized = "".join(tokens).replace("##", "")
         return detokenized
+
+
+class SubWordsTokenizer(BaseTokenizer):
+    vocabs = set()
+    disjoint_letters = """
+    ا
+    أ
+    إ
+    آ
+    ء
+    ؤ
+    ﻵ
+    ﻹ
+    ﻷ
+    د
+    ذ
+    ر
+    ز
+    و
+    ة
+    ى
+    """.split()
+
+    def tokenize(self, text):
+        for c in self.disjoint_letters:
+            modified_text = modified_text.replace(c, c + " ")
+        for word in modified_text.split():
+            self.vocabs.add(word.strip())
+        return modified_text
+
+    def tokens_list(self):
+        return list(self.vocabs)
+
+    def encode(self, text):
+        tokens_list = self.tokens_list()
+        return [
+            tokens_list.index(word.strip())
+            if word.strip() in self.vocabs
+            else self.unknown_token
+            for word in text.split()
+        ]
+
+    def decode(self, ids):
+        tokens_list = self.tokens_list()
+        # this needs to be fixed. It should return the index of <UNK> and <PAD> tokens.
+        return [tokens_list[id] if id < len(tokens_list) else "<UNK>" for id in ids]
+
+    def detokenize(self, tokenized):
+        """
+        This one is tricky!
+        """
+        pass
+
+
+# for the token_list, we should add the reserved tokens <PAD><UNK> to the first of the list
+# I think we should discuss this in more details.
