4531c542f875213dce8dc191df913e3aaa77931a
MagedSaeed <msmj2015@gmail.com>
2020-07-29 14:07:35 +0300

    adding long description

--- setup.py
+++ setup.py
@@ -4,13 +4,20 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+def readme():
+    with open('README.md', encoding="utf8") as f:
+        return f.read()
+
 setup(name='tkseem',
-      version='0.1',
+      version='0.0.1',
       description='Arabic Tokenization Library',
+      long_description_content_type='text/markdown',
+      long_description=readme(),
       url='https://github.com/MagedSaeed/tkseem',
-      author='Zaid Alyafeai',
-      author_email='alyafey22@gmail.com',
+      author='Zaid Alyafeai, Maged Saeed',
+      author_email='alyafey22@gmail.com, mageedsaeed1@gmail.com',
       license='MIT',
       packages=['tkseem'],
       install_requires=required,
+      python_requires=">=3.6",
       zip_safe=False)
