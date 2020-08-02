670b1e0afe4954e68510526c710d9a1568d538e7
MagedSaeed <msmj2015@gmail.com>
2020-08-02 04:46:54 +0300

    add check for train data path, remove abstracted methods

--- tkseem/character_tokenizer.py
+++ tkseem/character_tokenizer.py
@@ -1,3 +1,6 @@
+import re
+import pickle
+from collections import defaultdict
 from .__base import BaseTokenizer
 
 
@@ -9,6 +12,7 @@ class CharacterTokenizer(BaseTokenizer):
         """Train data using characters 
         """
         print("Training CharacterTokenizer ...")
+        self._check_train_data_path()
         rx = re.compile(r"\B(.)")
 
         text = open("data/raw/train.txt", "r").read()
@@ -41,67 +45,3 @@ class CharacterTokenizer(BaseTokenizer):
                 output_tokens.append(self.pad_token)
         return output_tokens
 
-    def load_model(self, file_path):
-        """Load a saved model as a frequency dictionary
-
-        Args:
-            file_path (str): file path of the dictionary
-        """
-        print("Loading as pickle file ...")
-        self.vocab = pickle.load(open(file_path, "rb"))
-
-    def save_model(self, file_path):
-        """Save a model as a freqency dictionary
-
-        Args:
-            file_path (str): file path to save the model
-        """
-        assert self.vocab
-        with open(f"{file_path}", "wb") as pickle_file:
-            print("Saving as pickle file ...")
-            pickle.dump(self.vocab, pickle_file)
-
-    def _tokens_list(self):
-        """ Get tokens list
-
-        Returns:
-            list: list of tokens.
-        """
-        return list(self.vocab.keys())
-
-    def decode(self, encoded):
-        """ Decode ids
-
-        Args:
-            encoded (list): list of ids to decode
-
-        Returns:
-            list: tokens
-        """
-        decoded = [self._tokens_list()[id] for id in encoded]
-        return decoded
-
-    def encode(self, text):
-        """ Convert string to a list of ids
-
-        Args:
-            text (str): input string
-
-        Returns:
-            list: list of ids
-        """
-        tokens = self.tokenize(text)
-        encoded = [self._tokens_list().index(token) for token in tokens]
-        return encoded
-
-    def detokenize(self, tokens):
-        """ Convert tokens to a string
-
-        Args:
-            tokens (list): list of tokens
-
-        Returns:
-            str: detokenized string
-        """
-        detokenized = "".join(tokens).replace("##", "")
-        return detokenized
