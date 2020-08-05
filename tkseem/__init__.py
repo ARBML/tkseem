1db886da9761f61a5bd7b87a6e2bbcf688252c45
zaidalyafeai <alyafey22@gmail.com>
2020-08-06 02:08:37 +0300

    revert to previous architecture

--- tkseem/__init__.py
+++ tkseem/__init__.py
@@ -1,16 +1,16 @@
-# from tkseem.tokenizers import (
-#     WordTokenizer,
-#     CharacterTokenizer,
-#     AutoTokenizer,
-#     CharacterTokenizer,
-#     DisjointLetterTokenizer,
-#     RandomTokenizer,
-#     SentencePieceTokenizer,
-# )
+from tkseem.tokenizers import (
+    WordTokenizer,
+    CharacterTokenizer,
+    AutoTokenizer,
+    CharacterTokenizer,
+    DisjointLetterTokenizer,
+    RandomTokenizer,
+    SentencePieceTokenizer,
+)
 
-from tkseem.word_tokenizer import WordTokenizer
-from tkseem.auto_tokenizer import AutoTokenizer
-from tkseem.random_tokenizder import RandomTokenizer
-from tkseem.disjoint_letters_tokenizer import DisjointLetterTokenizer
-from tkseem.sentencepiece_tokenizer import SentencePieceTokenizer
-from tkseem.character_tokenizer import CharacterTokenizer
+# from tkseem.word_tokenizer import WordTokenizer
+# from tkseem.auto_tokenizer import AutoTokenizer
+# from tkseem.random_tokenizder import RandomTokenizer
+# from tkseem.disjoint_letters_tokenizer import DisjointLetterTokenizer
+# from tkseem.sentencepiece_tokenizer import SentencePieceTokenizer
+# from tkseem.character_tokenizer import CharacterTokenizer
