05069e2594e94fb90237e6874797c11c2e8d9a55
MagedSaeed <msmj2015@gmail.com>
2020-08-16 21:23:34 +0300

    adding tests, incomplete

--- tkseem/test.py
+++
@@ -1,9 +0,0 @@
-from .util import remove_tashkeel
-import unittest
-
-class TestUnit(unittest.TestCase):
-    def test_tashkeel(self):
-        self.assertEqual(remove_tashkeel("مِكَرٍّ مِفَرٍّ مُقبِلٍ مُدبِرٍ مَعًا")
-                        , "مكر مفر مقبل مدبر معا", "Remove Tashkeel is not working")
-
-unittest.main(argv=['first-arg-is-ignored'], exit=False)
