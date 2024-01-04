8aac9d50d9087a9c56469755f3e6eca5048ee08b
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2024-01-04 10:05:26 -0500

    Refactor SentencePieceTokenizer train method to accept additional arguments

--- tkseem/sentencepiece_tokenizer.py
+++ tkseem/sentencepiece_tokenizer.py
@@ -6,31 +6,47 @@ from ._base import BaseTokenizer
 
 
 class SentencePieceTokenizer(BaseTokenizer):
-    """ Sentencepiece based tokenization.
-    """
+    """Sentencepiece based tokenization."""
 
-    def train(self, file_path, model_type="bpe"):
-        """ Train using sentence piece
+    def train(self, file_path, model_type="bpe", **kwargs):
+        """Train using sentence piece
 
         Args:
             file_path (str): file to train
             model_type (str, optional): train using sp. Defaults to "bpe".
+            kwargs: additional arguments to pass to the SentencePieceTrainer. See https://github.com/google/sentencepiece/blob/master/doc/options.md
         """
         print("Training SentencePiece ...")
         self.model = io.BytesIO()
+
+        if kwargs.get("vocab_size"):
+            print(
+                f"WARNING: Vocab size is being overwritten to {kwargs.get('vocab_size')}"
+            )
+            self.vocab_size = kwargs.get("vocab_size")
+
+        if kwargs.get("special_tokens"):
+            print(
+                f"WARNING: Special tokens are being overwritten to {kwargs.get('special_tokens')}"
+            )
+            self.special_tokens = kwargs.get("special_tokens")
 
         spm.SentencePieceTrainer.train(
             input=file_path,
             model_writer=self.model,
             vocab_size=self.vocab_size,
             model_type=model_type,
-            character_coverage=1.0,
+            character_coverage=kwargs.get("character_coverage", 1.0),
+            max_sentencepiece_length=kwargs.get("max_sentencepiece_length", 16),
             unk_id=0,
             pad_id=1,
             bos_id=-1,
             eos_id=-1,
             user_defined_symbols=self.special_tokens,
-            normalization_rule_name="identity",
+            train_extremely_large_corpus=kwargs.get(
+                "train_extremely_large_corpus", False
+            ),
+            normalization_rule_name=kwargs.get("normalization_rule_name", "identity"),
         )
         self.sp = spm.SentencePieceProcessor(model_proto=self.model.getvalue())
         self.vocab_size = self.sp.vocab_size()
@@ -71,7 +87,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         return self.sp.piece_to_id(token)
 
     def encode(self, text):
-        """ Convert string to a list of ids
+        """Convert string to a list of ids
 
         Args:
             text (str): input string
@@ -82,7 +98,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         return self.sp.encode(text, out_type=int)
 
     def decode(self, encoded):
-        """ Decode ids
+        """Decode ids
 
         Args:
             encoded (list): list of ids to decode
@@ -93,7 +109,7 @@ class SentencePieceTokenizer(BaseTokenizer):
         return self.sp.id_to_piece(encoded)
 
     def detokenize(self, tokens):
-        """ Convert tokens to a string
+        """Convert tokens to a string
 
         Args:
             tokens (list): list of tokens
